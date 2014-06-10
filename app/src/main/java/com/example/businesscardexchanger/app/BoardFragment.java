b34c726966ee7aeca49f945601fad910dcb17811
grosalex <alexbruneau93@gmail.com>
2014-06-10 21:33:00 +0200

    adding files

---
+++ app/src/main/java/com/example/businesscardexchanger/app/BoardFragment.java
@@ -0,0 +1,9 @@
+package com.example.businesscardexchanger.app;
+
+import android.app.Fragment;
+
+/**
+ * Created by grosalex on 10/06/14.
+ */
+public class BoardFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/businesscardexchanger/app/PlaceholderFragment.java
@@ -0,0 +1,29 @@
+package com.example.businesscardexchanger.app;
+
+/**
+ * Created by grosalex on 10/06/14.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+}
